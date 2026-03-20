55cf022acd3188f6b7452f29a8ee74dd263eaadb
roopashreed19 <roopashree.cs23@sahyadri.edu.in>
2026-03-20 08:42:18 +0530

    Fix CORS policy to allow Vercel origin

--- server/server.js
+++ server/server.js
@@ -13,8 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
-  credentials: true
+  origin: ["https://kodemaster-phi.vercel.app",'http://localhost:3000', 'http://localhost:3001'],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 // 3. Debugging Logs
