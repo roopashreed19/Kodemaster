6ae622ccc67a01244b18688b1652e42186a2dca3
roopashreed19 <roopashree.cs23@sahyadri.edu.in>
2026-04-02 23:42:42 +0530

    Changed origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: "https://kodemaster-phi.vercel.app",
+  credentials: true
+}));
 
 
 console.log("Checking URI...", process.env.MONGO_URI ? "found" : "NOT FOUND");
